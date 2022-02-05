62abeabe2d6bfe37a3f55aed4c6a75e88968c912
vladi2998 <vladimir.ac29@gmail.com>
2022-02-05 19:42:28 -0400

    The data is ordered but the dict does not look like the right result in the page

--- mean_var_std.py
+++ mean_var_std.py
@@ -7,12 +7,12 @@ def calculate(list):
     
     else:
         mat = np.matrix(list).reshape(3,3)
-        means = [mat.mean(1).tolist(), mat.mean(0).tolist(), mat.mean().tolist()]
-        varainces = [mat.var(1).tolist(), mat.var(0).tolist(), mat.var().tolist()]
-        stddesvs = [mat.std(1).tolist(), mat.std(0).tolist(), mat.std().tolist()]
-        maxs = [mat.max(1).tolist(), mat.max(0).tolist(), mat.max().tolist()]
-        mins = [mat.min(1).tolist(), mat.min(0).tolist(), mat.min().tolist()]
-        sums = [mat.sum(1).tolist(), mat.sum(0).tolist(), mat.sum().tolist()]
+        means = [mat.mean(0).tolist(), mat.mean(1).tolist(), mat.mean().tolist()]
+        varainces = [mat.var(0).tolist(), mat.var(1).tolist(), mat.var().tolist()]
+        stddesvs = [mat.std(0).tolist(), mat.std(1).tolist(), mat.std().tolist()]
+        maxs = [mat.max(0).tolist(), mat.max(1).tolist(), mat.max().tolist()]
+        mins = [mat.min(0).tolist(), mat.min(1).tolist(), mat.min().tolist()]
+        sums = [mat.sum(0).tolist(), mat.sum(1).tolist(), mat.sum().tolist()]
 
         calculations = {
             'mean': means,
