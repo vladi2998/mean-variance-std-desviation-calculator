fc31bf0da0f5a280f39334cb2c7213bb03a8fcc0
vladi2998 <vladimir.ac29@gmail.com>
2022-02-05 19:18:21 -0400

    Add the creation of matrix and the list that going to save all information for the dict result

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,22 @@ import numpy as np
 
 def calculate(list):
 
+    means = []
+    varainces = []
+    stddesvs = []
+    maxs = []
+    mins = []
+    sums = []
 
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    else:
+        mat = np.matrix(list).reshape(3,3)
+        
+    #return calculations
+    return mat 
 
-
-    return calculations
+test = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+mat = calculate(test)
+print(mat)
