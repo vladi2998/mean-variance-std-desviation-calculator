2fecd97315bcbbdde9bc33be4637e4df86147d3d
vladi2998 <vladimir.ac29@gmail.com>
2022-02-05 19:36:45 -0400

    All datas are calculated but the order is wrong in the dict

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,22 +2,28 @@ import numpy as np
 
 def calculate(list):
 
-    means = []
-    varainces = []
-    stddesvs = []
-    maxs = []
-    mins = []
-    sums = []
-
     if len(list) < 9:
         raise ValueError("List must contain nine numbers.")
     
     else:
         mat = np.matrix(list).reshape(3,3)
-        
-    #return calculations
-    return mat 
+        means = [mat.mean(1).tolist(), mat.mean(0).tolist(), mat.mean().tolist()]
+        varainces = [mat.var(1).tolist(), mat.var(0).tolist(), mat.var().tolist()]
+        stddesvs = [mat.std(1).tolist(), mat.std(0).tolist(), mat.std().tolist()]
+        maxs = [mat.max(1).tolist(), mat.max(0).tolist(), mat.max().tolist()]
+        mins = [mat.min(1).tolist(), mat.min(0).tolist(), mat.min().tolist()]
+        sums = [mat.sum(1).tolist(), mat.sum(0).tolist(), mat.sum().tolist()]
+
+        calculations = {
+            'mean': means,
+            'variance': varainces,
+            'standard deviation': stddesvs,
+            'max': maxs,
+            'min': mins,
+            'sum': sums
+        }
+    return calculations
 
 test = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-mat = calculate(test)
-print(mat)
+result = calculate(test)
+print(result)
