cb1306357bc777cd73ea7031e57e767ef81342e7
vladi2998 <vladimir.ac29@gmail.com>
2022-02-05 21:52:29 -0400

    The code works right

--- mean_var_std.py
+++ mean_var_std.py
@@ -6,13 +6,13 @@ def calculate(list):
         raise ValueError("List must contain nine numbers.")
     
     else:
-        mat = np.matrix(list).reshape(3,3)
-        means = [mat.mean(0).tolist(), mat.mean(1).reshape(1,3).tolist(), mat.mean().tolist()]
-        varainces = [mat.var(0).tolist(), mat.var(1).reshape(1,3).tolist(), mat.var().tolist()]
-        stddesvs = [mat.std(0).tolist(), mat.std(1).reshape(1,3).tolist(), mat.std().tolist()]
-        maxs = [mat.max(0).tolist(), mat.max(1).reshape(1,3).tolist(), mat.max().tolist()]
-        mins = [mat.min(0).tolist(), mat.min(1).reshape(1,3).tolist(), mat.min().tolist()]
-        sums = [mat.sum(0).tolist(), mat.sum(1).reshape(1,3).tolist(), mat.sum().tolist()]
+        mat = np.array(list).reshape(3,3)
+        means = [mat.mean(0).tolist(), mat.mean(1).reshape(1,3).tolist()[0], mat.mean().tolist()]
+        varainces = [mat.var(0).tolist(), mat.var(1).reshape(1,3).tolist()[0], mat.var().tolist()]
+        stddesvs = [mat.std(0).tolist(), mat.std(1).reshape(1,3).tolist()[0], mat.std().tolist()]
+        maxs = [mat.max(0).tolist(), mat.max(1).reshape(1,3).tolist()[0], mat.max().tolist()]
+        mins = [mat.min(0).tolist(), mat.min(1).reshape(1,3).tolist()[0], mat.min().tolist()]
+        sums = [mat.sum(0).tolist(), mat.sum(1).reshape(1,3).tolist()[0], mat.sum().tolist()]
 
         calculations = {
             'mean': means,
